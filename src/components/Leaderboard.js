eaf261be29e0ddb1bd31bc3791217bb6461e8e07
Marrick Somal <marrick.somal@gmail.com>
2020-12-10 16:12:39 +0000

    feat: Display Leaderboard with store data

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,41 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableBody from '@material-ui/core/TableBody';
-
-import '../styles/leaderboard.css'
+import Grid from '@material-ui/core/Grid';
 
 
-class LeaderBoard extends Component {
+import {useStyles} from '../styles/sharedStyles';
 
-    render() {
+
+function Leaderboard(props) {
+    const classes = useStyles();
+
+    const { userData } = props;
+    
         return (
-            <div className="leaderboard-container">
-                <Card>
-                <CardContent>
-                    <Table>
-                    <TableHead>
-                    <TableCell>Hi</TableCell>
-                    </TableHead>
-                    <TableBody>
-                    <TableRow>
-                        <TableCell rowSpan={2}>Avatar</TableCell>
-                        <TableCell >Answered questions</TableCell>
-                        <TableCell >Created questions</TableCell>
-                        <TableCell rowSpan={2} >Score</TableCell>
-                        </TableRow>
-                    </TableBody>
-                    </Table>
-                </CardContent>
-                </Card>
+            <div className={classes.toolbar}>
+            <div className={classes.root}>
+            {
+            userData.map((user, ix) => {
+            return <Card key={user.id}>
+            <div className={classes.header}>
+            {ix+1}. {user.name}
             </div>
+              <Grid container>
+                  <Grid item xs={4} className={classes.card}>
+                    <CardContent>
+                      <img alt=""
+                      src={user.avatarURL}
+                      width="300" 
+                      height="300"
+                      />
+                    </CardContent>
+                  </Grid>
+                  <Grid item xs = {6} className={classes.card}>
+                    <CardContent>
+                    <p>Answered questions: {user.answerCount}</p>
+                    <p>Created questions: {user.questionCount}</p>
+                    <p>Score: {user.totalContribution}</p>
+                    </CardContent>
+                  </Grid>
+              </Grid>
+            </Card>
+                })
+            }
+          </div>
+        </div>
         )
     }
-}
 
-export default LeaderBoard;
+    function mapStateToProps({ users }) {
+        const userData = Object.values(users)
+          .map(user => ({
+            id: user.id,
+            name: user.name,
+            avatarURL: user.avatarURL,
+            answerCount: Object.values(user.answers).length,
+            questionCount: user.questions.length,
+            totalContribution: Object.values(user.answers).length + user.questions.length
+          }))
+          .sort((a, b) => b.totalContribution - a.totalContribution)
+        return {
+          userData
+        };
+      }
+
+export default connect(mapStateToProps)(Leaderboard);
